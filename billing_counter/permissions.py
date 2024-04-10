d21d3e5181fdb5b1d4821a1cc6ffd1bc44fba5b0
tejaslakade99 <tejaslakade99@gmail.com>
2024-04-10 17:31:58 +0530

    Vercel Setup.

--- billing_counter/permissions.py
+++ billing_counter/permissions.py
@@ -5,8 +5,6 @@ from billing_counter.models import Employee
 class IsEmployee(BasePermission):
     def has_permission(self, request, view):
         employee = Employee.objects.get(user=request.user)
-        print("HElllo", employee.user.username)
         if employee:
             return True
-
         return False
